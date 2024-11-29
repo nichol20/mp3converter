07fa5e18b8de3ba8b7e12d4f75d9187bd7416093
Nicholas <nick.nogue28@outlook.com>
2024-11-29 10:55:19 -0300

    add ingress nginx script

---
+++ deploy-ingress-nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s

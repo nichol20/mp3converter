c450144dd8ed54deaa2f20f2a93d43ee5f6fce92
Nicholas <nick.nogue28@outlook.com>
2024-11-29 11:44:15 -0300

    fix login request

--- gateway/auth_svc/access.py
+++ gateway/auth_svc/access.py
@@ -7,7 +7,7 @@ def login(request):
 
     basicAuth = (auth.username, auth.password)
 
-    response = requests.post(f"http://{os.environ.get("AUTH_SVC_ADDRESS")}/login", auth=basicAuth)
+    response = requests.post(f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basicAuth)
 
     if response.status_code == 200:
         return response.text, None

74fdb442600a1aa6883bf93f3e64a5d44f972192
Nicholas <nick.nogue28@outlook.com>
2024-12-01 17:52:46 -0300

    add mp3 file id to message

--- converter/convert/to_mp3.py
+++ converter/convert/to_mp3.py
@@ -26,6 +26,8 @@ def start(message, fs_videos, fs_mp3s, channel):
     f.close()
     os.remove(tf_path)
 
+    message["mp3_fid"] = str(fid)
+
     try:
         channel.basic_publish(
             exchange="",
